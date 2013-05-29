70ed6ffc3d15f40009bc79cfc823709926ca351e
Pete Argent <peter.argent@gmail.com>
2013-05-29 15:20:09 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bumpcard::Application.config.secret_token = 'fc1e449ff865c6dafc7b324e5ce363024a71ff650e189b9bc6f824079714a2e9210749e34e34ffdae9a72b934e6e42b05168b8e9e9c4fa57de79be0dff156fa7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Bumpcard::Application.config.session_store :cookie_store, key: '_bumpcard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Bumpcard::Application.config.session_store :active_record_store
